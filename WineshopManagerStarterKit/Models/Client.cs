b7faa3032223b92aa32bed12c50653676b4e2499
LucaRabbit <luca.ramampy@gmail.com>
2026-03-17 16:13:41 +0100

    added Client model

---
+++ WineshopManagerStarterKit/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WineshopManagerStarterKit.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email format.")]
+        public string email { get; set; }
+
+        [StringLength(50)]
+        public string street { get; set; }
+
+        [RegularExpression(@"^\d{5}$", ErrorMessage = "Post code must be a 5-digit number.")]
+        public string postCode { get; set; }
+
+        public string city { get; set; }
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must be a 10-digit number.")]
+        public string phone { get; set; }
+    }
+}
