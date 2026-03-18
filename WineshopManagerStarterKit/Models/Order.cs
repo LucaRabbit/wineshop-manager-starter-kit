27b6fef7918bfbc3b7ca6331357951249944dede
Daalion <leojacquemet@gmail.com>
2026-03-18 11:00:00 +0100

    create model and controller

---
+++ WineshopManagerStarterKit/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace WineshopManagerStarterKit.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public Supplier supplier { get; set; }
+        public DateTime DateOrder { get; set; }
+        public DateTime DateDelivery { get; set; }
+        public string Street { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+    }
+}
